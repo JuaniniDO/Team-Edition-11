3bf2f34ce203ed0b7164cb75c52f4ffffc2ce444
JuanDomínguez <juandominortega92@gmail.com>
2023-02-21 13:37:02 +0100

    continius candidate singles

--- frontend/src/components/Img.js
+++ frontend/src/components/Img.js
@@ -5,7 +5,7 @@ import { faClock } from "@fortawesome/free-regular-svg-icons";
 
 function Img() {
   return (
-    <div className="col-lg-8 col-md-12 col-sm-12">
+    <div  className={classes["col-lg-8"]}>
       <div className={classes["content"]}>
         <div>
           <img src={img} alt="imagen" className={classes["img-candidate"]} />
@@ -31,28 +31,15 @@ function Img() {
             sint ratione, repellat impedit. Ut rerum earum saepe repellat omnis
             ducimus ipsam ipsa unde. Eius praesentium quibusdam ducimus iure
             inventore sint natus dignissimos necessitatibus autem mollitia
-            placeat dolores, iusto corrupti quod doloremque similique
-            repudiandae? Odio quidem expedita, laudantium minima necessitatibus
-            earum velit non ipsum doloremque illo error debitis voluptates
-            quisquam iusto quaerat. Dolore corporis perferendis atque quis
-            aperiam nesciunt, voluptate assumenda fugit natus eaque. Cumque
-            provident ullam aperiam vel quam porro voluptatem molestias eaque
-            officiis quos quasi, quibusdam obcaecati! Qui asperiores aut iusto
-            ratione. Quaerat voluptas quia voluptatem asperiores modi explicabo
-            deserunt voluptatibus doloribus?
+            placeat dolores.
+            
           </p>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia quas
             asperiores doloribus. Natus numquam sit, eum eius quisquam autem
             placeat fugit, voluptatum voluptates reprehenderit enim, quidem
             necessitatibus ratione iusto rerum. Itaque voluptatem facere eaque
-            aut! Sunt aspernatur eius aut. Totam repellat similique quia
-            explicabo non consectetur at ea laborum quas. Alias quis earum
-            recusandae quisquam a perspiciatis ullam optio non. Non debitis
-            odit, maiores optio ducimus quasi aperiam quia minima sit
-            distinctio, facilis excepturi deleniti eos cupiditate? Ad amet iusto
-            deserunt architecto, reprehenderit maxime sed hic aliquid, ea
-            dolorem minus.
+            aut! Sunt aspernatur eius aut. 
           </p>
         </div>
       </div>
